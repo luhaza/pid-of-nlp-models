26bb97225d257f2aa1ef4a2965b8f4b377a9fa9b
lukezann <luke.zanuck@gmail.com>
2024-11-30 09:27:59 -0800

    script to transform data into csv

--- data/sample_ibc/loadIBC.py
+++ data/sample_ibc/loadIBC.py
@@ -1,29 +1,41 @@
 import _pickle as cPickle
+import pandas as pd
 
 if __name__ == '__main__':
     [lib, con, neutral] = cPickle.load(open('sampleData.pkl', 'rb'))
 
-    # how to access sentence text
-    print('Liberal examples (out of ', len(lib), ' sentences): ')
-    for tree in lib[0:5]:
-        print(tree.get_words())
-
-    print('\nConservative examples (out of ', len(con), ' sentences): ')
-    for tree in con[0:5]:
-        print(tree.get_words())
-
-    print('\nNeutral examples (out of ', len(neutral), ' sentences): ')
-    for tree in neutral[0:5]:
-        print(tree.get_words())
+    data = pd.DataFrame(columns=["sentence", "label"])
+    index = 0
 
     # how to access phrase labels for a particular tree
     ex_tree = lib[0]
 
-    print('\nPhrase labels for one tree: ')
+    # can choose to only use the full sentence, or traverse the tree
+    full_tree = False
+
+    ids = [lib, con, neutral]
 
     # see treeUtil.py for the tree class definition
-    for node in ex_tree:
+    for id in ids:
+        for tree in id:
+            for node in tree:
 
-        # remember, only certain nodes have labels (see paper for details)
-        if hasattr(node, 'label'):
-            print(node.label, ': ', node.get_words())
+                # remember, only certain nodes have labels (see paper for details)
+                if hasattr(node, 'label'):
+                    data.loc[index, "sentence"] = node.get_words()
+                    label = node.label
+
+                    # if label == 'Conservative':
+                    #     result = -1
+                    # elif label == 'Neutral':
+                    #     result = 0
+                    # else:
+                    #     result = 1
+                    data.loc[index, "label"] = label
+
+                    index += 1
+
+                    if not full_tree:
+                        break
+            
+    data.to_csv("sample_ibc.csv")
